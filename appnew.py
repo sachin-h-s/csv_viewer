006cf2307b8f424b4f181c10494f218a27d8a223
sachin-h-s <89412649+sachin-h-s@users.noreply.github.com>
2023-01-09 12:20:17 +0530

    Update appnew.py

--- appnew.py
+++ appnew.py
@@ -28,11 +28,13 @@ if uploaded_file is not None:
     st.dataframe(df)
 
     # Allow the user to download the modified CSV
-    st.markdown("""
-    ## Download modified CSV
-    """)
-    if st.button("Download modified CSV"):
-        csv = df.to_csv(index=False)
-        b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-        href = f'<a href="data:file/csv;base64,{b64}">Download modified CSV file</a>'
-        st.markdown(href, unsafe_allow_html=True)
+# Allow the user to download the modified CSV
+st.markdown("""
+## Download modified CSV
+""")
+if st.button("Download modified CSV"):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="modified.csv">Download modified CSV file</a>'
+    st.markdown(href, unsafe_allow_html=True)
+
