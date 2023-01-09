4362ccfd6d05de84ca90482131a2437c9d4fc506
sachin-h-s <89412649+sachin-h-s@users.noreply.github.com>
2023-01-09 11:21:19 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+st.title('CSV Viewer')
+
+uploaded_file = st.file_uploader('Choose a CSV file', type='csv')
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.table(df)
