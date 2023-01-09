375a4ccde07dd80c16cf57b841c9503f3abbcc57
sachin-h-s <89412649+sachin-h-s@users.noreply.github.com>
2023-01-09 11:26:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ uploaded_file = st.file_uploader('Choose a CSV file', type='csv')
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
-    st.table(df)
     st.write('Shape:', df.shape)
+    st.table(df)
+    
 
